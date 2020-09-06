4c1748e78813a937d2ce7c584fa3088dc507abad
Shichimin <35617411+Shichimin@users.noreply.github.com>
2020-09-07 00:50:10 +0900

    [update]仮レイアウトの作成

---
+++ src/components/About.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <h1 class="content-title"><font-awesome-icon icon="user-circle" class="icon"/> About</h1>
+    <div>
+      <div class="about">
+        <div class="me">
+          <h2><font-awesome-icon icon="pen" class="icon"/> Name</h2>
+          <p class="text">Tomoki Kawakami</p>
+        </div>
+
+        <div class="me">
+          <h2><font-awesome-icon icon="birthday-cake" class="icon"/> Birthday</h2>
+          <p class="text">1992-04-14</p>
+        </div>
+
+        <div class="me">
+          <h2><font-awesome-icon icon="search" class="icon"/> Recent interests</h2>
+          <p class="text">Flutter, Vue.js</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<style>
+.about{
+    display:inline-flex;
+    padding-left: 5%;
+    padding-right: 5%;
+    
+}
+.resent{
+  margin-top: 50px;
+}
+.me{
+    margin:0 50px;
+}
+@media screen and (max-width: 768px){
+	.about {
+		flex-direction: column;
+	}
+	.me {
+    margin: 0 auto;
+	}
+}
+</style>
--- src/components/Skill.vue
+++ src/components/Skill.vue
@@ -1,6 +1,77 @@
 <template>
-  <v-container>
-    <h1>一覧画面画面</h1>
-    <router-link to="/">Go to Home</router-link>
-  </v-container>
+  <div>
+    <h1 class="content-title"><font-awesome-icon icon="code" class="icon"/> Skill</h1>
+    <div>
+      <div class="languages">
+        <div class="lang">
+          <img src="../assets/lang/html-5.svg" alt="logo" class="lang-image">
+          <h3 class="lang-comment">HTML</h3>
+        </div>
+        <div class="lang">
+          <img src="../assets/lang/css-3.svg" alt="logo" class="lang-image">
+          <h3>CSS</h3>
+        </div>
+        <div class="lang">
+          <img src="../assets/lang/javascript.svg" alt="logo" class="lang-image">
+          <h3>JavaScript</h3>
+        </div>
+        <div class="lang">
+          <img src="../assets/lang/vue.svg" alt="logo" class="lang-image">
+          <h3>Vue.js</h3>
+        </div>
+        <div class="lang">
+          <img src="../assets/lang/vuetify.png" alt="logo" class="lang-image">
+          <h3>Vuetify</h3>
+        </div>
+        <div class="lang">
+          <img src="../assets/lang/swift.png" alt="logo" class="lang-image">
+          <h3>Swift</h3>
+        </div>
+        <div class="lang">
+          <img src="../assets/lang/ios.png" alt="logo" class="lang-image">
+          <h3>iOS</h3>
+        </div>
+        <div class="lang">
+          <img src="../assets/lang/ruby.png" alt="logo" class="lang-image">
+          <h3>Ruby</h3>
+        </div>
+        <div class="lang">
+          <img src="../assets/lang/rails.png" alt="logo" class="lang-image">
+          <h3>Rails</h3>
+        </div>
+        <div class="lang">
+          <img src="../assets/lang/git.svg" alt="logo" class="lang-image">
+          <h3>Git</h3>
+        </div>
+        <div class="lang">
+          <img src="../assets/lang/github.svg" alt="logo" class="lang-image">
+          <h3>GitHub</h3>
+        </div>
+        <div class="lang">
+          <img src="../assets/lang/writing.png" alt="logo" class="lang-image">
+          <h5>Scenario Writing</h5>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
+
+<style>
+.languages{
+    text-align: center;
+    padding-left: 15%;
+    padding-right: 15%;
+}
+.lang{
+    margin :10px 30px;
+    display: inline-block;
+    width: 100px;
+}
+.lang-comment{
+    width: 100px;
+}
+.lang-image{
+    height: 50px;
+    width: 50px;
+}
+</style>
